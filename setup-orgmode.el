9436687c40c2481ad2de5a8d82b8eff3e3fcfd41
William Denton <wtd@pobox.com>
2013-04-19 00:08:39 -0400

    Trying to get inline images working in org-mode

--- setup-orgmode.el
+++ setup-orgmode.el
@@ -5,4 +5,21 @@
 ; Hide the /italics/ and *bold* markers
 (setq org-hide-emphasis-markers t)
 
+; Hit return on a link to open it in a browser
+(setq org-return-follows-link t)
+
+(setq org-display-inline-images t)
+
+; active Babel languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+   (latex . t)
+   (R . t)
+   (ruby . t)
+   (sh . t)
+   (sql . t)
+   )
+ )
+
 (provide 'setup-orgmode)
