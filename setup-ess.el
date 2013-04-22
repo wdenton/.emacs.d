2737e9fa8cf6580014695e0a200483597b5dce49
William Denton <wtd@pobox.com>
2013-04-22 10:00:48 -0400

    Org mode fiddling

--- setup-ess.el
+++ setup-ess.el
@@ -1,9 +1,13 @@
 ;; Emacs Speaks Statistics (for R)
+
+; (autoload 'ess-site "ess-site" "R" t)
 (require 'ess-site)
 (setq ess-use-auto-complete t)
 ;; Make R go to the width of the Emacs frame
 (add-hook 'ess-R-post-run-hook 'ess-execute-screen-options)
 
+
+; (autoload 'ess-R-object-popup "ess-R-object-popup" "Get info for object at point, and display it in a popup" t)
 (require 'ess-R-object-popup)
 (define-key ess-mode-map "\C-c\C-g" 'ess-R-object-popup)
 
--- setup-orgmode.el
+++ setup-orgmode.el
@@ -8,7 +8,20 @@
 ; Hit return on a link to open it in a browser
 (setq org-return-follows-link t)
 
+; Embed an image with [[file:foo.png]] and then C-c C-x C-v to view
 (setq org-display-inline-images t)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+; Capturing
+(setq org-default-notes-file "~/notes.org") ; Change this when I use it for real
+(define-key global-map "\C-cc" 'org-capture)
+(setq org-capture-templates
+      '(("w" "Work todo" entry (file+headline "~/york/gtd/next-actions.org" "Tasks") "* TODO %?\n %u\n %a"))
+      )
+
 
 ; active Babel languages
 (org-babel-do-load-languages
