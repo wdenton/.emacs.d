681aa9c5407e6de7c0f236427344dff2404c98aa
William Denton <wtd@pobox.com>
2014-01-14 18:54:13 -0500

    Shortcuts and key bindings for Jekyll ease

--- setup-jekyll.el
+++ setup-jekyll.el
@@ -6,4 +6,59 @@
   (when (region-active-p) (delete-region (region-beginning) (region-end) ) )
   (insert (format-time-string "%Y-%m-%d %H:%M:%S %z")))
 
+;; All of the below is taken from http://www.gorgnegre.com/linux/using-emacs-orgmode-to-blog-with-jekyll.html
+
+(global-set-key (kbd "C-c C-j n") 'jekyll-draft-post)
+(global-set-key (kbd "C-c C-j p") 'jekyll-publish-post)
+(global-set-key (kbd "C-c C-j t") 'jekyll-timestamp)
+; (global-set-key (kbd "C-c j p") (lambda () (interactive) (find-file "~/web/_posts/")))
+; (global-set-key (kbd "C-c j d") (lambda () (interactive) (find-file "~/web/_drafts/")))
+
+(defvar jekyll-directory "~/web/" "Path to Jekyll blog.")
+(defvar jekyll-drafts-dir "_drafts/" "Relative path to drafts directory.")
+(defvar jekyll-posts-dir "_posts/" "Relative path to posts directory.")
+(defvar jekyll-post-ext ".md"  "File extension of Jekyll posts.")
+(defvar jekyll-post-template "---\nlayout: post\ntitle: %s\ntags:\npublished: false\n---\n"
+  "Default template for Jekyll posts. %s will be replace by the post title.")
+
+(defun jekyll-make-slug (s) "Turn a string into a slug."
+  (replace-regexp-in-string " " "-"  (downcase (replace-regexp-in-string "[^A-Za-z0-9 ]" "" s))))
+
+(defun jekyll-yaml-escape (s) "Escape a string for YAML."
+  (if (or (string-match ":" s) (string-match "\"" s)) (concat "\"" (replace-regexp-in-string "\"" "\\\\\"" s) "\"") s))
+
+(defun jekyll-draft-post (title) "Create a new Jekyll blog post."
+  (interactive "sPost Title: ")
+  (let ((draft-file (concat jekyll-directory jekyll-drafts-dir
+                            (jekyll-make-slug title)
+                            jekyll-post-ext)))
+    (if (file-exists-p draft-file)
+        (find-file draft-file)
+      (find-file draft-file)
+      (insert (format jekyll-post-template (jekyll-yaml-escape title))))))
+
+(defun jekyll-publish-post () "Move a draft post to the posts directory, and rename it so that it contains the date."
+  (interactive)
+  (cond
+   ((not (equal
+          (file-name-directory (buffer-file-name (current-buffer)))
+          (expand-file-name (concat jekyll-directory jekyll-drafts-dir))))
+    (message "This is not a draft post.")
+    (insert (file-name-directory (buffer-file-name (current-buffer))) "\n"
+            (concat jekyll-directory jekyll-drafts-dir)))
+   ((buffer-modified-p)
+    (message "Can't publish post; buffer has modifications."))
+   (t
+    (let ((filename
+           (concat jekyll-directory jekyll-posts-dir
+                   (format-time-string "%Y-%m-%d-")
+                   (file-name-nondirectory
+                    (buffer-file-name (current-buffer)))))
+          (old-point (point)))
+      (rename-file (buffer-file-name (current-buffer))
+                   filename)
+      (kill-buffer nil)
+      (find-file filename)
+      (set-window-point (selected-window) old-point)))))
+
 (provide 'setup-jekyll)
