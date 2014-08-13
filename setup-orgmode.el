f885f772077d0eca59eed90f5ff9b3541538fad6
William Denton <wdenton@yorku.ca>
2014-08-13 11:41:41 -0400

    Adjust height of level 1 headings

--- setup-orgmode.el
+++ setup-orgmode.el
@@ -17,6 +17,15 @@
 ;; Hide the /italics/ and *bold* markers
 (setq org-hide-emphasis-markers t)
 
+;; Change the sizes of the titles.
+(custom-set-faces
+  '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+)
+
 ;; Better colouring of TODO keywords
 (setq org-todo-keyword-faces
       (quote (
