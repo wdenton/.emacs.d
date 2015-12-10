9f98ecda437ca889b9a329846ab99ed28217fac2
William Denton <wtd@pobox.com>
2015-12-10 00:15:57 -0500

    More elfeed tweaks

--- setup/setup-elfeed.el
+++ setup/setup-elfeed.el
@@ -4,11 +4,16 @@
 
 ;; https://github.com/skeeto/elfeed
 
-(load-file "~/.emacs.d/setup/setup-elfeed-subscriptions.el")
-
 (require 'elfeed)
 
+;; See example file for what this should look like
+(load-file "~/.emacs.d/setup/setup-elfeed-subscriptions.el")
+
+;; Show date as "10 Dec 2015"
 (defun elfeed-search-format-date (date)
   (format-time-string "%d %b %Y" (seconds-to-time date)))
+
+;; Stop feed fetches from timing out so quickly
+(setf url-queue-timeout 30)
 
 (provide 'setup-elfeed)
