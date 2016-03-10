58dffd66f73d0b0dd03076141d3bbcbc0c326d62
William Denton <wtd@pobox.com>
2016-03-10 15:35:17 -0500

    Hack to make sonic-pi.el work

--- setup/setup-sonic-pi.el
+++ setup/setup-sonic-pi.el
@@ -6,9 +6,12 @@
 
 (add-hook 'sonic-pi-mode-hook
 	  (lambda ()
-	    (setq sonic-pi-path "/usr/local/src/sonic-pi/")
- 	    ;; (setq sonic-pi-path "/usr/local/src/sonic-pi-2.6.0/")
+	    (setq sonic-pi-server-bin             "app/server/bin/sonic-pi-server.rb")
+	    (setq sonic-pi-compile-extensions-bin "app/server/bin/compile-extensions.rb")
+	    (setq sonic-pi-path                   "/usr/local/src/sonic-pi/")
 	    (define-key ruby-mode-map "\C-c\C-c" 'sonic-pi-send-buffer)))
+
+(setq false nil)
 
 ;; .spi seems to be the suffix for Sonic Pi scripts
 (setq auto-mode-alist (cons '("\\.spi" . ruby-mode) auto-mode-alist))
