23699b1b6353b1b2d39d6f71fdf3eb14f71b489b
William Denton <wdenton@yorku.ca>
2016-05-05 11:03:10 -0400

    Font again

--- init.el
+++ init.el
@@ -167,7 +167,8 @@
 ;;;;
 
 ;; (set-face-attribute 'default nil :height 110) ;; Measured in 0.1 pts
-(set-face-attribute 'default nil :font "DejaVu Sans Mono" :height 100) ;; Measured in 0.1 pts
+;; (set-face-attribute 'default nil :font "DejaVu Sans Mono Book" :height 110) ;; Measured in 0.1 pts
+(set-face-attribute 'default nil :font "Ubuntu Mono" :height 120) ;; Measured in 0.1 pts
 
 ;;;;
 ;;;; Theme: solarized
