bbc0ae54e340d763d1323f3de42b264ad4fa8483
William Denton <wtd@pobox.com>
2016-05-06 14:13:59 -0400

    Finally done with the damn font size?

--- init.el
+++ init.el
@@ -168,7 +168,7 @@
 
 ;; (set-face-attribute 'default nil :height 110) ;; Measured in 0.1 pts
 ;; (set-face-attribute 'default nil :font "DejaVu Sans Mono Book" :height 110) ;; Measured in 0.1 pts
-(setq wtd-font-height-for-this-machine 110)
+(setq wtd-font-height-for-this-machine 105)
 (when (string= system-name "work")
   ;; Need it a little bigger on my work machine
   (setq wtd-font-height-for-this-machine 115))
