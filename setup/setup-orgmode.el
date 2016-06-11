27ff97028de10442eee18ad3aca6a40701ae573d
William Denton <wtd@pobox.com>
2016-06-11 10:23:09 -0400

    Prettify BEGIN_SRC

--- setup/setup-orgmode.el
+++ setup/setup-orgmode.el
@@ -175,10 +175,13 @@
 	(insert description))))
 
 ;; Hooks for prettify-symbols-mode
+;; See also https://pank.eu/blog/pretty-babel-src-blocks.html for some cool stuff
 (add-hook 'org-mode-hook
  	  (lambda ()
  	    (push '("<=" . ?≤) prettify-symbols-alist)
  	    (push '(">=" . ?≥) prettify-symbols-alist)
+ 	    (push '("#+BEGIN_SRC" . ?✎) prettify-symbols-alist)
+ 	    (push '("#+END_SRC" . ?□) prettify-symbols-alist)
  	    ))
 
 ;; Capturing
