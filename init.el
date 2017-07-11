27aa53a46ba765a5aa7fb0616cbd30f6627c4b94
William Denton <wtd@pobox.com>
2017-07-11 10:44:51 -0400

    Tweaked swiper setup

--- init.el
+++ init.el
@@ -722,25 +722,27 @@ already narrowed."
 ;;;;
 
 ;; https://github.com/abo-abo/swiper
+;; See also http://irreal.org/blog/?p=6419
 
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-;; (global-set-key (kbd "C-c j") 'counsel-git-grep) ;; I use C-c j for Jekyll
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+(use-package swiper
+  :ensure t
+  :diminish ivy-mode
+  :bind (("C-s" . swiper)
+         ("C-c C-r" . ivy-resume)
+         ("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-M-i" . complete-symbol)
+         ("C-." . counsel-imenu)
+         ("C-c 8" . counsel-unicode-char)
+         ("C-c g" . counsel-git)
+         ("C-c k" . counsel-ag)
+         ("C-c v" . ivy-push-view)
+         ("C-c V" . ivy-pop-view)
+         ("M-y" . counsel-yank-pop))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "%d/%d "))
 
 ;;;;
 ;;;; Projectile (https://github.com/bbatsov/projectile)
