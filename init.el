88a03df327c189ec702cdc358bc8fffe6300ccc7
William Denton <wtd@pobox.com>
2019-03-03 22:33:21 -0500

    Use all-the-icons in dired

--- init.el
+++ init.el
@@ -810,6 +810,12 @@ already narrowed."
   (all-the-icons-ivy-setup)
   )
 
+(use-package all-the-icons-dired
+  :ensure t
+  :config
+  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+  )
+
 ;;;;
 ;;;; which-key
 ;;;;
