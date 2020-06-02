7e78b1f6ce75cb2abdeca416ea7a5b584014ee80
William Denton <wtd@pobox.com>
2020-06-01 21:41:09 -0400

    Add folding to nxml mode

--- init.el
+++ init.el
@@ -744,6 +744,27 @@ already narrowed."
   )
 
 ;;;;
+;;;; nXML
+;;;;
+
+;; Why isn't folding built in?
+;; Taken from https://emacs.stackexchange.com/a/3157/145
+
+;; (require 'hideshow)
+;; (require 'sgml-mode)
+;; (require 'nxml-mode)
+
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
+;;;;
 ;;;; Mode-specific customizations
 ;;;;
 
