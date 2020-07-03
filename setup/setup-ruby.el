5737fc3e7d53237e342afe6626e0825cb4b29daf
William Denton <wtd@pobox.com>
2020-07-03 01:21:15 -0400

    Try Robe with Ruby

--- setup/setup-ruby.el
+++ setup/setup-ruby.el
@@ -12,21 +12,25 @@
 ;; I use rbenv, so Emacs needs to know about it
 ;; https://github.com/senny/rbenv.el
 (use-package rbenv
+  :hook (ruby-mode . global-rbenv-mode)
   :config
   (setq rbenv-show-active-ruby-in-modeline nil)
   (setq rbenv-modeline-function 'rbenv--modeline-plain)
-  (add-hook 'ruby-mode-hook 'global-rbenv-mode)
+  (rbenv-use-global)
   )
 
 ;; Open up irb with M-x inf-ruby or C-c C-s from a Ruby buffer
 (use-package inf-ruby
+  :hook (ruby-mode . inf-ruby-minor-mode)
   :config
   (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-  (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
   )
 
 ;; Avoid ridiculous Ruby indentation.
 (setq ruby-deep-indent-paren nil)
+
+;; Don't put the UTF-8 encoding comment at the top
+(setq ruby-insert-encoding-magic-comment nil)
 
 ;; Parentheses.
 (require 'smartparens-ruby)
@@ -39,8 +43,17 @@
 ;; Rubocop for pointing out errors (https://github.com/bbatsov/rubocop)
 (use-package rubocop
   :diminish rubocop-mode
+  :hook ruby-mode
+  )
+
+;; Robe for documentation lookup, etc.
+;; https://github.com/dgutov/robe
+;; Requires gems pry and pry-doc
+(use-package robe
+  :diminish robe-mode
+  :hook ruby-mode
   :config
-  (add-hook 'ruby-mode-hook 'rubocop-mode)
+  (eval-after-load 'company '(push 'company-robe company-backends))
   )
 
 ;; indent-guide ... very nice
